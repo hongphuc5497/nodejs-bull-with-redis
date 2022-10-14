e051dce68a08b3bd4fe7e4f7d266ef8471505156
Hong Phuc <hongphuc.dthp@gmail.com>
2022-10-15 01:37:29 +0700

    feat(index): use Redis to cache

--- index.js
+++ index.js
@@ -2,26 +2,56 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const Queue = require('bull');
+const { createClient } = require('redis');
 
-const PORT = process.env.PORT || 8888;
+let redisClient;
+(async () => {
+	try {
+		redisClient = createClient();
+
+		await redisClient.connect();
+	} catch (err) {
+		console.error('Redis Client Error', err);
+	}
+})();
 
 app.get('/fish-species', async (req, res) => {
 	try {
-		const API_URL = 'https://www.fishwatch.gov/api/species';
-		const { data } = await axios.get(API_URL);
+		const CACHE_KEY = 'fish-species';
+		let species;
 		let isCached = false;
+
+		const isExist = await redisClient.EXISTS(CACHE_KEY);
+		if (isExist) {
+			isCached = true;
+
+			const cachedData = await redisClient.LRANGE(CACHE_KEY, 0, -1);
+			species = cachedData.map((item) => JSON.parse(item));
+		} else {
+			const API_URL = 'https://www.fishwatch.gov/api/species';
+			const { data } = await axios.get(API_URL);
+
+			await Promise.all(
+				data.map((element) => {
+					redisClient.RPUSH(CACHE_KEY, JSON.stringify(element));
+				})
+			);
+
+			species = data;
+		}
 
 		return res.status(200).json({
 			fromCache: isCached,
-			data,
+			data: species,
 		});
 	} catch (err) {
-		console.err(err);
+		console.error(err);
 
 		return res.status(404).send('Data unavailable');
 	}
 });
 
+const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}`);
 });
