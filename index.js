bb58a361ae3a5cc7b7b3e06a0357054ab9798d20
Hong Phuc <hongphuc.dthp@gmail.com>
2022-10-14 17:18:17 +0700

    feat: add basic queues

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const bull = require('bull');
+const Queue = require('bull');
 const { createClient } = require('redis');
 
 (async () => {
-  try {
-    const redisClient = createClient()
+	try {
+		const basicQueue = new Queue('basic-queue');
+		await basicQueue.add({
+			name: 'Test',
+			age: 20,
+		});
+		basicQueue.process((job, done) => {
+			console.log(job.data);
+			done();
+		});
 
-    await redisClient.connect();
-  } catch (err) {
-    console.log('Redis Client Error', err)
-  }
+		const scheduleQueue = new Queue('schedule-queue', {
+			defaultJobOptions: {
+				repeat: { every: 1000 * 60 * 5 },
+			},
+		});
+		await scheduleQueue.add({});
+		scheduleQueue.process((_, done) => {
+			console.log('Schedule job');
+			done();
+		});
+	} catch (err) {
+		// console.log('Redis Client Error', err);
+		console.log('Bull Job Error', err);
+	}
 })();
