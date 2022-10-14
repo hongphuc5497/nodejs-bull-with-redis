73372f487a6bb060bd97ea82da648e5f380abb3d
Hong Phuc <hongphuc.dthp@gmail.com>
2022-10-15 00:51:44 +0700

    feat: add fetch mock data API

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
 const Queue = require('bull');
 
-(async () => {
-	try {
-		const basicQueue = new Queue('basic-queue');
-		await basicQueue.add({
-			name: 'Test',
-			age: 20,
-		});
-		basicQueue.process((job, done) => {
-			console.log(job.data);
-			done();
-		});
+const PORT = process.env.PORT || 8888;
 
-		const scheduleQueue = new Queue('schedule-queue', {
-			defaultJobOptions: {
-				repeat: { every: 1000 * 60 * 5 },
-			},
-		});
-		await scheduleQueue.add({});
-		scheduleQueue.process((_, done) => {
-			console.log('Schedule job');
-			done();
+app.get('/fish-species', async (req, res) => {
+	try {
+		const API_URL = 'https://www.fishwatch.gov/api/species';
+		const { data } = await axios.get(API_URL);
+		let isCached = false;
+
+		return res.status(200).json({
+			fromCache: isCached,
+			data,
 		});
 	} catch (err) {
-		console.log('Bull Job Error', err);
+		console.err(err);
+
+		return res.status(404).send('Data unavailable');
 	}
-})();
+});
+
+app.listen(PORT, () => {
+	console.log(`App listening on port ${PORT}`);
+});
+
+// (async () => {
+// 	try {
+// 		const basicQueue = new Queue('basic-queue');
+// 		await basicQueue.add({
+// 			name: 'Test',
+// 			age: 20,
+// 		});
+// 		basicQueue.process((job, done) => {
+// 			console.log(job.data);
+// 			done();
+// 		});
+
+// 		const scheduleQueue = new Queue('schedule-queue', {
+// 			defaultJobOptions: {
+// 				repeat: { every: 1000 * 60 * 5 },
+// 			},
+// 		});
+// 		await scheduleQueue.add({});
+// 		scheduleQueue.process((_, done) => {
+// 			console.log('Schedule job');
+// 			done();
+// 		});
+// 	} catch (err) {
+// 		console.log('Bull Job Error', err);
+// 	}
+// })();
